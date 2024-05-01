f267451db44000632ac6f819af45e9e1e75ad4f3
Cristian David Campos Duran <134449312+Cristian-Universidad@users.noreply.github.com>
2024-05-01 12:30:11 -0500

    Muchos cambios
    Cambie Script, Agregue movimientos de los personajes y sus respectivos sonidos

--- v.1.0 ejemplo/Assets/Scripts/PlayerMovementLuigi.cs
+++ v.1.0 ejemplo/Assets/Scripts/PlayerMovementLuigi.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementLuigi : MonoBehaviour
 {
 
     public float Speed;
@@ -26,10 +26,10 @@ public class PlayerMovement : MonoBehaviour
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Horizontal < 0.0f) transform.localScale = new Vector3(-0.4898234f, 0.4898234f, 0.4898234f);
-        else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.4898234f, 0.4898234f, 0.4898234f);
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-0.5565351f, 0.5565351f, 0.5565351f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.5565351f, 0.5565351f, 0.5565351f);
 
-       Animator.SetBool("running", Horizontal != 0.0f);  /*Animacion*/
+        Animator.SetBool("running", Horizontal != 0.0f);  /*Animacion*/
 
         if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
         {
@@ -51,7 +51,7 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal*Speed, Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
     }
 
 }
